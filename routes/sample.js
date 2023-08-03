85966480aa15cbc724b1d769070791453a599889
Tetsuya Kawano <tetsuya.kawano@gmail.com>
2023-08-03 11:51:21 +0900

    add error pattern

--- routes/sample.js
+++ routes/sample.js
@@ -16,11 +16,17 @@ router.get('/hello', function(req, res, next) {
 });
 
 router.post('/hello', function(req, res, next) {
-    greet = "hello, " + req.body.name + "!"
-    var param = {"restext":greet};
-    res.header('Content-Type', 'application/json; charset=utf-8')
-    res.send(param);
-  });
+  var r = Math.floor(Math.random() * 11);
+  if (r <= 2) {
+    res.status(500);
+    res.end('たまにエラーを起こしています');
+  }
+
+  greet = "hello, " + req.body.name + "!"
+  var param = {"restext":greet};
+  res.header('Content-Type', 'application/json; charset=utf-8')
+  res.send(param);
+});
 
 
 module.exports = router;
